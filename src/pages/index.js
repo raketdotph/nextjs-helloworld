e9a5bf67bb9a4ac20873ea5b1499af137330d4bd
Team RAKET <62831116+raketdotph@users.noreply.github.com>
2024-06-29 10:14:41 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello World -Team Raket :) {" "}
       <Link href="/about">
         About
       </Link>
